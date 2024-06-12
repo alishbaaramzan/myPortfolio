c2916d18e3da087b4dd695c179007312387d81c3
alishbaaramzan <wizzz1211@gmail.com>
2024-06-12 14:07:07 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// header scroll
+// we will now write code to make sure the window is any of the sections and not the area between the sections
+let nav = document.querySelector(".navbar");
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide in mobile view
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
